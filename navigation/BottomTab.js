3a0471e7996657bf64b7145a3666e69d962f3cd9
Mikorolo <mikolaj.widanka@gmail.com>
2022-02-02 15:31:06 +0100

    Added icons to the bottom tab

--- navigation/BottomTab.js
+++ navigation/BottomTab.js
@@ -1,6 +1,7 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Notifications from "../views/bottomTab/Notifications";
 import TopTab from "./TopTab";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
@@ -8,9 +9,29 @@ function BottomTab() {
     return (
         <Tab.Navigator>
             <Tab.Screen name="TopTab" component={TopTab} options={{
-                headerShown: false
+                headerShown: false,
+                tabBarIcon: (tabInfo) => {
+                    return (
+                        <Ionicons
+                            name="md-cart"
+                            size={24}
+                            color={tabInfo.focused ? "#4C52DE" : "#8e8e93"}
+                        />
+                    );
+                },
             }}/>
-            <Tab.Screen name="Notifications" component={Notifications} options={{ headerShown: false }} />
+            <Tab.Screen name="Notifications" component={Notifications} options={{
+                headerShown: false,
+                tabBarIcon: (tabInfo) => {
+                    return (
+                        <Ionicons
+                            name="md-notifications"
+                            size={24}
+                            color={tabInfo.focused ? "#4C52DE" : "#8e8e93"}
+                        />
+                    );
+                },
+            }} />
         </Tab.Navigator>
     );
 }
